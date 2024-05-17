6519b74b5990096a5cbf840278a33419bcb10d19
lutheranderson <kongneandersonluther@gmail.com>
2024-05-17 17:12:35 +0100

    premier pas sur lacceuil

--- components/Codet.js
+++ components/Codet.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, FlatList, Image, ScrollView } from 'react-native'
+import { DATA } from '../data/dataImage'
 
-const Codet = () => {
+const Codet = () => { 
   return (
-    <View>
-      <Text>Codet</Text>
+    <View style={styles.container}>
+      <View style={styles.entete}>
+        <Text>Faire un don</Text>
+        <Text>Login</Text>
+      </View>
+      <ScrollView>
+        <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+      </ScrollView>
+      <View>
+        <FlatList
+            data={DATA}
+            horizontal
+            showsHorizontalScrollIndicator='false'
+            renderItem={({item})=>(
+                <Image source={{uri:item.img}} style={styles.image} />
+            )}
+        />
+      </View>
     </View>
   )
-}
+} 
+
+const styles = StyleSheet.create({
+    title:{
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginTop: 20
+    },
+    container:{
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    image:{
+        height: 100,
+        width: 100,
+        margin: 5
+    },
+    entete:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
 
 export default Codet
---
+++ data/dataImage.js
@@ -0,0 +1,17 @@
+export const DATA =  [
+    {
+        img: 'https://cdn.pixabay.com/photo/2022/11/12/15/43/reed-7587271_640.jpg'
+    }, 
+    {
+        img: 'https://cdn.pixabay.com/photo/2023/06/19/18/41/blue-jay-8075346_640.jpg'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2024/01/09/20/41/wheat-8498380_640.jpg'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2024/05/07/03/49/agave-8744705_640.jpg'
+    },
+    {
+        img: 'https://cdn.pixabay.com/photo/2018/06/06/11/21/cape-robin-chat-3457709_640.jpg'
+    }
+]
